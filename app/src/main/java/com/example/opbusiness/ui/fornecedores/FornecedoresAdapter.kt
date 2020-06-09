3ecbc964b1516bacf7ca501b8fde3891a85e6439
carlosivis <carlosivis1@gmail.com>
2020-06-09 13:11:42 -0300

    fornecedores parcialmente implementado

---
+++ app/src/main/java/com/example/opbusiness/ui/fornecedores/FornecedoresAdapter.kt
@@ -0,0 +1,3 @@
+package com.example.opbusiness.ui.fornecedores
+
+class FornecedoresAdapter
--- app/src/main/java/com/example/opbusiness/ui/fornecedores/FornecedoresFragment.kt
+++ app/src/main/java/com/example/opbusiness/ui/fornecedores/FornecedoresFragment.kt
@@ -1,4 +1,4 @@
-package com.example.opbusiness.ui
+package com.example.opbusiness.ui.fornecedores
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,13 +8,13 @@ import android.view.ViewGroup
 import com.example.opbusiness.R
 
 
-class ContatosFragment : Fragment() {
+class FornecedoresFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false)
+        return inflater.inflate(R.layout.fragment_fornecedores, container, false)
     }
 }
